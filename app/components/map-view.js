89394dc742122b2dabdac1f196069e05ccb14640
allthesignals <wilburnforce@gmail.com>
2015-08-28 13:54:12 -0400

    Refactored map component to be leaner and more modular.

--- app/components/map-view.js
+++ app/components/map-view.js
@@ -2,53 +2,33 @@ import Ember from 'ember';
 // import ClipperLib from 'npm:js-clipper';
 
 export default Ember.Component.extend({
-  geom: null,
-  geoJsonLayer: null,
-  initialize: function() {
-    console.log("Initialized");
-  },
+  previousLayer: new L.featureGroup(),
+
+  geometry: function () {
+    return new L.geoJson(this.get("geom"));
+  }.property('geom'),
+
   updateMap: function() {
-    var geoJsonLayer = this.get('geoJsonLayer');
-    var data = this.get('geom.geometry');
-    geoJsonLayer.clearLayers();
-    geoJsonLayer.addData(data);
+    var previousLayer = this.get('previousLayer');
+    var geometry = this.get('geometry');
 
-  }.observes('geom'),
-  updateScope: function() {
-    this.map.fitBounds(this.geoJsonLayer.getBounds());
-  }.observes('geom'),
-  makeSqlPolygon: function(coords) {
-    var s = "ST_SETSRID(ST_PolygonFromText(\'POLYGON((";
-    var firstCoord;
-    coords.forEach(function(coord,i){
-      s+=coord.lng + " " + coord.lat + ",";
+    previousLayer.clearLayers();
+    geometry.addTo(this.map);
+    this.map.fitBounds(geometry.getBounds());
+    this.set('previousLayer', geometry);
 
-      //remember the first coord
-      if(i==0) {
-        firstCoord = coord;
-      }
+  }.observes('geometry'),
 
-      if(i==coords.length-1) {
-        s+=firstCoord.lng + " " + firstCoord.lat;
-      }
-    });
-    s+="))\'),4326)"
-    return s;
-  },
   didInsertElement: function() {
+    //initial DOM rendering
     this.map = L.map(this.$('#map').get(0));
-
-    var geoJsonLayer = L.geoJson().addTo(this.map);
-
-    this.set('geoJsonLayer', geoJsonLayer);
-    var data = this.get('geom.geometry');
-    geoJsonLayer.addData(data);
-    this.map.fitBounds(geoJsonLayer.getBounds());
-    this.map.invalidateSize();
     
     L.tileLayer('http://{s}.tile.stamen.com/toner-background/{z}/{x}/{y}.png', {
       attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
       subdomains: 'abcd',
     }).addTo(this.map);
+
+    this.updateMap();
+
   }
 });
