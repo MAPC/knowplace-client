d73485dd7ac2000c6d4f452bc8568fe7b2ae0129
allthesignals <wilburnforce@gmail.com>
2015-10-16 15:41:35 -0400

    Fixed filtering issue, wasn't looking up the right property.

--- app/components/random-data.js
+++ app/components/random-data.js
@@ -3,7 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   randomData: function() {
     if (this.get('data')) {
-      return this.get('data').sort(function() { return 0.5 - Math.random() }).filter(function(el) { return el !=="evaluated-data-collection";  }).objectsAt([1,2,3]);
+      return this.get('data')
+              .sort(function() { return 0.5 - Math.random() })
+              .filter(function(el) { return el.type !=="evaluated-data-collection";  })
+              .objectsAt([1,2,3]);
     }
   }.property('data')
 });
